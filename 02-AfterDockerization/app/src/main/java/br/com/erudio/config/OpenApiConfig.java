43685e1ecdfde52873212fef6f150b918740d0c8
leandrocgsi <leandrocgsi@gmail.com>
2024-01-11 13:21:22 -0300

    App After Dockerization

--- 02-AfterDockerization/app/src/main/java/br/com/erudio/config/OpenApiConfig.java
+++ 02-AfterDockerization/app/src/main/java/br/com/erudio/config/OpenApiConfig.java
@@ -14,7 +14,7 @@ public class OpenApiConfig {
 	OpenAPI customOpenAPI() {
 		return new OpenAPI()
 			.info(new Info()
-				.title("Docker from Zero to Mastery - Demystified Containerization!")
+                .title("Docker to AWS with Java!")
 				.version("v1")
 				.description("Some description about your API")
 				.termsOfService("https://pub.erudio.com.br/meus-cursos")
